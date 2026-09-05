2cdbbd929d6d5b04dfae9f37eeca65a5ecf888d8
Bruno <bruno880078@gmail.com>
2026-09-05 10:54:00 -0300

    retirada de uma função não usada no repositório user

--- internal/usecase/userusecase/user_repository.go
+++ internal/usecase/userusecase/user_repository.go
@@ -11,7 +11,6 @@ type UserRepository interface {
 	FindByID(id uuid.UUID) (*user.User, error)
 	FindByEmail(email string) (*user.User, error)
 	DeleteByID(id uuid.UUID) error
-	ExistByID(id uuid.UUID) (bool, error)
 	ExistsByEmail(email string) (bool, error)
 	ChangeOwner(callerID uuid.UUID, targetID uuid.UUID) error
 }
